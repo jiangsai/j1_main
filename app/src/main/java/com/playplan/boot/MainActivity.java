bdfb182613681b58cef709c89c45aec59c3d8a1e
jyt <125839128@qq.com>
2021-07-06 15:58:46 +0800

    测试提交到哪里去
    Change-Id: I19f3bad80104a479a443a882ce37d07a1f2f4ad6

--- app/src/main/java/com/playplan/boot/MainActivity.java
+++ app/src/main/java/com/playplan/boot/MainActivity.java
@@ -4,7 +4,8 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.playplan.x.Test;
+import com.playplan.architecture_view.Test;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,6 +13,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Test.dd();
+        Test.test();
     }
 }
