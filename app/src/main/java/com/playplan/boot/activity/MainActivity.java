2a6cbcf01f77f661e0a908d4b43ab0e965386a04
jyt <125839128@qq.com>
2021-10-14 11:30:29 +0800

    类名修改
    Change-Id: I80a2f2c081afb7dae57a5179cab0547ae7f3599d

---
+++ app/src/main/java/com/playplan/boot/activity/MainActivity.java
@@ -0,0 +1,60 @@
+package com.playplan.boot.activity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.playplan.boot.databinding.ActivityMainBinding;
+
+
+public class MainActivity extends AppCompatActivity {
+    ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+
+        ActivityResultLauncher launcher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), result -> {
+                    switch (result.getResultCode()) {
+                        case 0:
+                            Intent intent = result.getData();
+                            if (intent != null) {
+                                Log.e("jyt", "-----onActivityResult" + intent.getStringExtra("111"));
+                            }
+                            break;
+                    }
+
+                }
+        );
+
+        binding.tv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                PermissionX.init(MainActivity.this)
+//                        .permissions(Manifest.permission.READ_CONTACTS,
+//                                Manifest.permission.CAMERA,
+//                                Manifest.permission.CALL_PHONE)
+//                        .request((allGranted, grantedList, deniedList) -> {
+//                            if (allGranted) {
+//                                ToastUtils.show("All permissions are granted");
+//                            } else {
+//                                ToastUtils.show("These permissions are denied: $deniedList");
+//                            }
+//                        });
+                launcher.launch(new Intent(getBaseContext(), ProcessTestActivity.class));
+
+
+            }
+        });
+
+    }
+}
--- app/src/main/java/com/playplan/boot/activity/ProcessTestActivity.java
+++ app/src/main/java/com/playplan/boot/activity/ProcessTestActivity.java
@@ -1,60 +1,79 @@
 package com.playplan.boot.activity;
 
+import android.content.ComponentName;
 import android.content.Intent;
+import android.content.ServiceConnection;
 import android.os.Bundle;
+import android.os.IBinder;
+import android.os.RemoteException;
 import android.util.Log;
 import android.view.View;
 
-import androidx.activity.result.ActivityResultLauncher;
-import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.playplan.boot.databinding.ActivityMainBinding;
+import com.hjq.toast.ToastUtils;
+import com.playplan.boot.databinding.ActivityProcessTestBinding;
+import com.playplan.boot.process.IMyAidlInterface;
+import com.playplan.boot.process.IMyCallBack;
+import com.playplan.boot.process.MyData;
 
 
 public class ProcessTestActivity extends AppCompatActivity {
-    ActivityMainBinding binding;
+    private ActivityProcessTestBinding binding;
+    private IMyAidlInterface iMyAidlInterface;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        binding = ActivityProcessTestBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
-
-
-        ActivityResultLauncher launcher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), result -> {
-                    switch (result.getResultCode()) {
-                        case 0:
-                            Intent intent = result.getData();
-                            if (intent != null) {
-                                Log.e("jyt", "-----onActivityResult" + intent.getStringExtra("111"));
-                            }
-                            break;
-                    }
-
-                }
-        );
-
-        binding.tv.setOnClickListener(new View.OnClickListener() {
+        binding.c.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-//                PermissionX.init(MainActivity.this)
-//                        .permissions(Manifest.permission.READ_CONTACTS,
-//                                Manifest.permission.CAMERA,
-//                                Manifest.permission.CALL_PHONE)
-//                        .request((allGranted, grantedList, deniedList) -> {
-//                            if (allGranted) {
-//                                ToastUtils.show("All permissions are granted");
-//                            } else {
-//                                ToastUtils.show("These permissions are denied: $deniedList");
-//                            }
-//                        });
-                launcher.launch(new Intent(getBaseContext(), TwoActivity.class));
-
-
+//                setResult(0, new Intent().putExtra("111", "x"));
+//                finish();
+                try {
+                    ToastUtils.show(iMyAidlInterface.getString());
+                    iMyAidlInterface.setData(new MyData("666"));
+                } catch (RemoteException e) {
+                    e.printStackTrace();
+                }
             }
         });
 
+        Intent intent = new Intent();
+        intent.setAction("com.playplan.boot.process");
+        intent.setPackage("com.playplan.boot");
+
+        ComponentName componentName = new ComponentName(getPackageName(), "com.playplan.boot.process.MyService");
+        intent.setComponent(componentName);
+
+        bindService(intent, new ServiceConnection() {
+
+            @Override
+            public void onServiceConnected(ComponentName name, IBinder service) {
+
+                iMyAidlInterface = IMyAidlInterface.Stub.asInterface(service);
+                try {
+                    iMyAidlInterface.setListener(stub);
+                } catch (RemoteException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void onServiceDisconnected(ComponentName name) {
+
+            }
+        }, BIND_AUTO_CREATE);
+        startService(intent);
+
     }
+
+    IMyCallBack.Stub stub = new IMyCallBack.Stub() {
+        @Override
+        public void callData() {
+            Log.e("jyt", "callData");
+        }
+    };
 }
